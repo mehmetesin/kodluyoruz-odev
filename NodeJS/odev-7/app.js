2f049b09bc7e5aeb9c02839e1519aa7636a65340
Mehmet Eşin <mehmetesin@gmail.com>
2023-01-04 12:15:37 +0300

    NodeJS | odev 8

--- NodeJS/odev-7/app.js
+++ NodeJS/odev-7/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.set('view engine', 'ejs');
 
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog desription"
-  }
-  res.send(blog)
-})
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add_post');
+});
+//<--Routes
 
 const port = 3000;
 app.listen(port, () => {
